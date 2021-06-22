89d75d5b5277fda1c818ea040e3c0a5e7140c915
tawhidmonowar <tawhidmonowar@gmail.com>
2021-06-23 00:11:39 +0600

    New console calculator added

---
+++ Console Calculator.c
@@ -0,0 +1,59 @@
+/*
+Simple Console Calculator
+Author: Tawhid Monowar
+*/
+
+
+#include <stdio.h>
+int main()
+{
+    int n,x,y,z;
+    float d;
+
+    printf("This is Simple Console Calculator, Author: Tawhid Monowar\n");
+    printf("\nPress 1 for Summation.\nPress 2 for Subtraction.\nPress 3 for Multiplication.\nPress 4 for Division.\n");
+    scanf("%d",&x);
+
+    if(x==1)
+    {
+        printf("Enter The First Number: ");
+        scanf("%d",&y);
+        printf("Enter The Second Number: ");
+        scanf("%d",&z);
+        n = y+z;
+        printf("The Answer is: %d", n);
+
+    } else if (x==2)
+    {
+        printf("Enter The First Number: ");
+        scanf("%d",&y);
+        printf("Enter The Second Number: ");
+        scanf("%d",&z);
+        n = y-z;
+        printf("The Answer is: %d", n);
+
+    } else if (x==3)
+    {
+        printf("Enter The First Number: ");
+        scanf("%d",&y);
+        printf("Enter The Second Number: ");
+        scanf("%d",&z);
+        n = y*z;
+        printf("The Answer is: %d", n);
+
+    } else if (x==4)
+    {
+        printf("Enter The First Number: ");
+        scanf("%d",&y);
+        printf("Enter The Second Number: ");
+        scanf("%d",&z);
+        d = (float)y/z;
+        printf("The Answer is: %.5f", d);
+
+    } else
+    {
+        printf("You Entered a Wrong Number! Try Again");
+    }
+
+    return 0;
+}
