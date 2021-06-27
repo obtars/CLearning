4a717e37a528dcb570b93c1f730361c0e71f4f5d
tawhidmonowar <tawhidmonowar@gmail.com>
2021-06-27 23:13:36 +0600

    6-27-21

--- main.c
+++ main.c
@@ -1,24 +1,6 @@
-/*
-Convert minutes to years, days and seconds
-Author: Tawhid Monowar
-*/
-
 #include <stdio.h>
 int main()
 {
-    int minutes,minutesInaYear = 525600,minutesInaDay = 1440;
-    double days,years,seconds;
-
-    printf("Convert minutes to years, days and seconds, Author: Tawhid Monowar\n\n");
-    printf("Enter the number of minutes:");
-    scanf("%d",&minutes);
-
-    years = minutes/ (double)minutesInaYear;
-    printf("\n%d minutes = %f Years\n",minutes,years);
-    days = minutes/ (double)minutesInaDay;
-    printf("%d minutes = %f Days\n",minutes,days);
-    seconds = minutes * 60;
-    printf("%d minutes = %f Seconds\n\n",minutes,seconds);
 
 
 
