8fe9281a9bd8e70c885ed396b80ab45923b358ae
tawhidmonowar <tawhidmonowar@gmail.com>
2021-07-03 17:09:08 +0600

    7-3-2021

--- main.c
+++ main.c
@@ -1,9 +1,22 @@
-#include <stdio.h>
+#include<stdio.h>
 int main()
 {
+   int grades [10];
+   int i;
+   int sum;
+   float ava = 0.0f;
 
+   printf("Enter The Grades:\n");
 
+   for (i=0; i<10; i++)
+   {
+       printf("%2u=",i+1);
+       scanf("%d",&grades[i]);
+       sum += grades[i];
+   }
+   ava = (float)sum/10;
 
-    return 0;
+   printf("The Ava is:%.2f",ava);
+
+   return 0;
 }
-
