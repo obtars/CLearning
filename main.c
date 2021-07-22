264cf2cc69dd64bfb9c1272de43598ffa88fd042
tawhidmonowar <tawhidmonowar@gmail.com>
2021-07-22 22:26:30 +0600

    7-22-0201

--- main.c
+++ main.c
@@ -1,34 +1,24 @@
-/*Author: Tawhid Monowar*/
-
 #include<stdio.h>
+#include<string.h>
 int main()
 {
-    int i,j,k,n=1;
 
-    for(i=1;i<=5;i++)
+    char name[] = "";
+    char copy[] = "";
+
+    scanf("%s",name);
+    scanf("%s",copy);
+
+
+    if (strcmp(name,copy)==1)
     {
-        for(j=1,k=1;k<=15;j++,k++)
-        {
-            if(i==1||((i==3)&&(j<=5))||i==5||j==1||j==10||j==15)
-            {
-                if(j==6||j==11||(j==9)&&(i==3))
-                {
-                    printf("  ");
-                }
-                printf("*");
-            }
-            else
-            {
-                printf(" ");
-            }
+        printf("Boro");
+    }else
+    {
+        printf("Suto");
 
-            if ((j==15)&&(i==2)||(j==15)&&(i==3)||(j==15)&&(i==4))
-            {
-                printf("    *");
-            }
-        }
-     printf("\n");
     }
+
 
     return 0;
 }
