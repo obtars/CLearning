5809be97adaf7db5b26ce143c55596bb296dbeac
tawhidmonowar <tawhidmonowar@gmail.com>
2021-08-28 19:55:26 +0600

    28-8-2021

--- main.c
+++ main.c
@@ -1,47 +1,20 @@
 #include <stdio.h>
-#define SIZE 10
+
 int main()
 {
-    int aMatrix[SIZE][SIZE], bMatrix[SIZE][SIZE];
-    int amRow,amCol,bmRow,bmCol;
-    int i,j;
+    int x,i,sum=0;
+    printf("How many number do you want to enter? ");
+    scanf("%d",&x);
+    int n[x];
+    printf("Entre The Numbers One by one.\n");
+    for (i=1;i<=x;i++)
+    {
+        scanf("%d",&n[i]);
+        sum = sum + n[i];
+    }
 
-    printf("Enter The Number of Rows and columns for A Matrix\n");
-    scanf("%d%d",&amRow,&amCol);
-    printf("Enter The Number of Rows and columns for B Matrix\n");
-    scanf("%d%d",&bmRow,&bmCol);
-
-        printf("Enter The Value of A matrix\n"); //A Matrix er value input
-
-        for (i=0;i<amRow;i++)
-        {
-            for (j=0;j<amCol;j++)
-            {
-            scanf("%d",&aMatrix[i][j]);
-            }
-        }
-
-
-    printf("Enter The Value of B matrix\n"); //B Matrix er value input
-        for (i=0;i<bmRow;i++)
-        {
-            for (j=0;j<bmCol;j++)
-            {
-                scanf("%d",&bMatrix[i][j]);
-            }
-        }
-
-
-    printf("Result: \n"); //Result mMatrix print
-
-    for (i=0;i<amRow;i++)
-        {
-          for (j=0;j<bmCol;j++)
-          {
-              printf("%d\t", aMatrix[i][j]+bMatrix[i][j]);
-          }
-          printf("\n");
-        }
+    printf("Sum = %d\n",sum);
+    printf("Avarage = %.3f",(float)sum/x);
 
     return 0;
 }
