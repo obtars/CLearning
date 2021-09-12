1320bb9e6a01d756a11a193439380404628b01c9
tawhidmonowar <tawhidmonowar@gmail.com>
2021-09-12 23:44:03 +0600

    12-09-2021

--- main.c
+++ main.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
+#include <stddef.h>
 int main()
 {
-    int n = 5, *i = &n;
-    printf("%d", sizeof(*i));
+    int n = 5;
+    int *i = &n;
+    int *janina = NULL;
+
+    printf("Value: %d\n",n);
+    printf("Value Address: %p\n",&n);
+    printf("Pointer: %p\n",i);
+    printf("Pointer Value: %d\n",*i);
+    printf("Pointer Address: %p\n",&i);
+
+    if(janina)
+    {
+        printf("Value Ase!");
+    }else
+    {
+        printf("Value Nai!");
+    }
+
+    return 0;
 }
