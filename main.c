de4810d5424a387db2009f9abf7de60909c7beac
tawhidmonowar <tawhidmonowar@gmail.com>
2021-09-12 23:00:21 +0600

    12-09-2021

--- main.c
+++ main.c
@@ -1,20 +1,6 @@
 #include <stdio.h>
 int main()
 {
-    int n;
-    printf("Enter your mark");
-    scanf("%d",&n);
-    if (n>0 && n<100)
-    {
-        if (n<33)
-            printf("F");
-        else if (n<=50)
-        {
-            printf("C");
-        }
-    }
-    else
-        printf("You entred a worng nomber");
-
-
+    int n = 5, *i = &n;
+    printf("%d", sizeof(*i));
 }
