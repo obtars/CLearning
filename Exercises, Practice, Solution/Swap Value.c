1bb63d3888cbb1520739ff1db9034a2d74c84a88
tawhidmonowar <tawhidmonowar@gmail.com>
2021-09-13 22:52:03 +0600

    13-09-2021

--- Exercises, Practice, Solution/Swap Value.c
+++ Exercises, Practice, Solution/Swap Value.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int swapData(int a, int b){
+int swapValue(int a, int b){
     int temp;
 
     temp = a;
@@ -15,6 +15,6 @@ int main(){
 
     int a = 5, b = 8;
     printf("a = %d\nb = %d\n\n",a,b);
-    swapData(a,b);
+    swapValue(a,b);
     return 0;
 }
